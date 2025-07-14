45573a179afedc99017271a83e09248c7f684285
gjwnddns <hju7826@gmail.com>
2025-07-14 12:52:56 +0900

    250714배너 완료

--- js/script.js
+++ js/script.js
@@ -118,6 +118,20 @@ categoryMenus.forEach(menu => {
     });
 });
 
+//categories__submenu hover 시,
+//categories__submenu--hovered 클래스 추가
+const categorySubmenus = document.querySelectorAll('.categories__submenu');
+
+categorySubmenus.forEach(submenu => {
+    submenu.addEventListener('mouseenter', () => {
+        submenu.classList.add('categories__submenu--hovered');
+    });
+
+    submenu.addEventListener('mouseleave', () => {
+        submenu.classList.remove('categories__submenu--hovered');
+    });
+});
+
 // 다크모드 버튼 hover 시,
 // 다크모드 버튼--hovered 클래스 추가
 const darkMode = document.querySelector('.dark-mode');
@@ -165,3 +179,41 @@ if (darkModeOff) {
         localStorage.setItem('darkMode', 'false');
     });
 }
+
+//banner__navigation__event-more 클릭시,
+//event-modal--active 클래스 제거, 추가가
+const eventMore = document.querySelector('.banner__navigation__event-more');
+const eventModal = document.querySelector('.event-modal');
+const eventModalClose = document.querySelector('.event-modal__close');
+
+eventMore.addEventListener('click', () => {
+    eventModal.classList.add('event-modal--active');
+});
+
+eventModalClose.addEventListener('click', () => {
+    eventModal.classList.remove('event-modal--active');
+});
+
+//event-modal__close hover 시,
+//event-modal__close--hovered 클래스 추가
+eventModalClose.addEventListener('mouseenter', () => {
+    eventModalClose.classList.add('event-modal__close--hovered');
+});
+
+eventModalClose.addEventListener('mouseleave', () => {
+    eventModalClose.classList.remove('event-modal__close--hovered');
+});
+
+//.event-item hover 시,
+//.event-item--hovered 클래스 추가
+const eventItems = document.querySelectorAll('.event-item');
+
+eventItems.forEach(item => {
+    item.addEventListener('mouseenter', () => {
+        item.classList.add('event-item--hovered');
+    });
+
+    item.addEventListener('mouseleave', () => {
+        item.classList.remove('event-item--hovered');
+    });
+});
